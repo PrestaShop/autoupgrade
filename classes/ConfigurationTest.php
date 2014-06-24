0a8cdc5a029926092c102fa7e71aa94d402607f3
zimmi1 <zimmi73@gmail.com>
2014-06-24 09:00:49 +0200

    Remove $this->l() in static class
    No $this in static classes. Need to find an other way to translate those two sentences.

--- classes/ConfigurationTest.php
+++ classes/ConfigurationTest.php
@@ -102,7 +102,7 @@ class ConfigurationTestCore
 		$dir = rtrim(_PS_ROOT_DIR_, '\\/').DIRECTORY_SEPARATOR.trim($relative_dir, '\\/');
 		if (!file_exists($dir) || !$dh = opendir($dir))
 		{
-			$full_report = sprintf($this->l('Directory %s does not exists or is not writable'), $dir); // sprintf for future translation
+			$full_report = sprintf('Directory %s does not exists or is not writable', $dir); // sprintf for future translation
 			return false;
 		}
 		$dummy = rtrim($dir, '\\/').DIRECTORY_SEPARATOR.uniqid();
@@ -117,7 +117,7 @@ class ConfigurationTestCore
 		}
 		elseif (!is_writable($dir))
 		{
-			$full_report = sprintf($this->l('Directory %s is not writable'), $dir); // sprintf for future translation
+			$full_report = sprintf('Directory %s is not writable', $dir); // sprintf for future translation
 			return false;
 		}
 
