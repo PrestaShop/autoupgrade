900e7a69157c7cda089a26323cb1d929efe9f415
Maxime Biloé <maxime.biloe@gmail.com>
2017-03-15 11:47:06 +0100

    // Version updated to v2.0.5

--- autoupgrade.php
+++ autoupgrade.php
@@ -31,7 +31,7 @@ class Autoupgrade extends Module
         $this->name = 'autoupgrade';
         $this->tab = 'administration';
         $this->author = 'PrestaShop';
-        $this->version = '2.0.4';
+        $this->version = '2.0.5';
         $this->need_instance = 1;
 
         $this->bootstrap = true;
