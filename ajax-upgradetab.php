0d1181bb0f3bd3d7665a35d5e2e32ae9f600b786
Thomas Nabord <thomas.nabord@prestashop.com>
2018-02-26 17:08:07 +0000

    Introduce TaskRepository

--- ajax-upgradetab.php
+++ ajax-upgradetab.php
@@ -25,6 +25,7 @@
 */
 
 use PrestaShop\Module\AutoUpgrade\Tools14;
+use PrestaShop\Module\AutoUpgrade\UpgradeTools\TaskRepository;
 
 require_once(realpath(dirname(__FILE__).'/../../modules/autoupgrade').'/ajax-upgradetabconfig.php');
 autoupgrade_ajax_init(dirname(__FILE__));
@@ -39,80 +40,8 @@ if (!$adminObj->checkToken()) {
     die(1);
 }
 
-switch (Tools14::getValue('action')) {
-    // MISCELLANEOUS (upgrade configuration, checks etc.)
-    case 'checkFilesVersion':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Miscellaneous\CheckFilesVersion($adminObj);
-        break;
-    case 'compareReleases':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Miscellaneous\CompareReleases($adminObj);
-        break;
-    case 'getChannelInfo':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Miscellaneous\GetChannelInfo($adminObj);
-        break;
-    case 'updateConfig':
-        $controller = new \PrestaShop\Module\AutoUpgrade\TaskRunner\Miscellaneous\UpdateConfig($adminObj);
-        break;
-
-    // ROLLBACK
-    case 'noRollbackFound':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Rollback\NoRollbackFound($adminObj);
-        break;
-    case 'restoreDb':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Rollback\RestoreDb($adminObj);
-        break;
-    case 'restoreFiles':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Rollback\RestoreFiles($adminObj);
-        break;
-    case 'rollback':
-        $controller = new \PrestaShop\Module\AutoUpgrade\TaskRunner\Rollback\Rollback($adminObj);
-        break;
-    case 'rollbackComplete':
-        $controller = new \PrestaShop\Module\AutoUpgrade\TaskRunner\Rollback\RollbackComplete($adminObj);
-        break;
-
-    // UPGRADE
-    case 'backupDb':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\BackupDb($adminObj);
-        break;
-    case 'backupFiles':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\BackupFiles($adminObj);
-        break;
-    case 'cleanDatabase':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\CleanDatabase($adminObj);
-        break;
-    case 'download':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\Download($adminObj);
-        break;
-    case 'removeSamples':
-        $controller = new \PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\RemoveSamples($adminObj);
-        break;
-    case 'upgradeComplete':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\UpgradeComplete($adminObj);
-        break;
-    case 'upgradeDb':
-        $controller = new \PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\UpgradeDb($adminObj);
-        break;
-    case 'upgradeFiles':
-        $controller = new \PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\UpgradeFiles($adminObj);
-        break;
-    case 'upgradeModules':
-        $controller = new \PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\UpgradeModules($adminObj);
-        break;
-    case 'upgradeNow':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\UpgradeNow($adminObj);
-        break;
-    case 'unzip':
-        $controller = new PrestaShop\Module\AutoUpgrade\TaskRunner\Upgrade\Unzip($adminObj);
-        break;
-
-    default:
-        error_log('Action '.Tools14::getValue('action').' is unknown!', 0);
-}
-
-if ($controller instanceof \PrestaShop\Module\AutoUpgrade\TaskRunner\AbstractTask) {
-    $controller->run();
-}
+$controller = TaskRepository::get(Tools14::getValue('action'), $adminObj);
+$controller->run();
 
 $adminObj->displayAjax();
-
+exit(0);
