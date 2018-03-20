475da99417972e21c4330b2d6253753202a989c6
Thomas Nabord <thomas.nabord@prestashop.com>
2018-03-20 17:04:54 +0000

    Remove listFilesToUpgrade from AdminSelfUpgrade

--- classes/TaskRunner/Upgrade/UpgradeFiles.php
+++ classes/TaskRunner/Upgrade/UpgradeFiles.php
@@ -35,77 +35,11 @@ class UpgradeFiles extends AbstractTask
     {
         $this->upgradeClass->nextParams = $this->upgradeClass->currentParams;
 
-        $admin_dir = str_replace($this->upgradeClass->prodRootDir.DIRECTORY_SEPARATOR, '', $this->upgradeClass->adminDir);
-        if (file_exists($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'admin')) {
-            rename($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'admin', $this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.$admin_dir);
-        } elseif (file_exists($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'admin-dev')) {
-            rename($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'admin-dev', $this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.$admin_dir);
-        }
-        if (file_exists($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'install-dev')) {
-            rename($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'install-dev', $this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'install');
-        }
-
+        /*
+         * The first call must init the list of files be upgraded
+         */
         if (!$this->upgradeClass->getFileConfigurationStorage()->exists(UpgradeFileNames::toUpgradeFileList)) {
-            // list saved in UpgradeFileNames::toUpgradeFileList
-            // get files differences (previously generated)
-            $admin_dir = trim(str_replace($this->upgradeClass->prodRootDir, '', $this->upgradeClass->adminDir), DIRECTORY_SEPARATOR);
-            $filepath_list_diff = $this->upgradeClass->autoupgradePath.DIRECTORY_SEPARATOR.UpgradeFileNames::diffFileList;
-            $list_files_diff = array();
-            if (file_exists($filepath_list_diff)) {
-                $list_files_diff = $this->upgradeClass->getFileConfigurationStorage()->load(UpgradeFileNames::diffFileList);
-                // only keep list of files to delete. The modified files will be listed with _listFilesToUpgrade
-                $list_files_diff = $list_files_diff['deleted'];
-                foreach ($list_files_diff as $k => $path) {
-                    if (preg_match("#autoupgrade#", $path)) {
-                        unset($list_files_diff[$k]);
-                    } else {
-                        $list_files_diff[$k] = str_replace('/'.'admin', '/'.$admin_dir, $path);
-                    }
-                } // do not replace by DIRECTORY_SEPARATOR
-            }
-
-            if (!($list_files_to_upgrade = $this->upgradeClass->_listFilesToUpgrade($this->upgradeClass->latestRootDir))) {
-                return false;
-            }
-
-            // also add files to remove
-            $list_files_to_upgrade = array_merge($list_files_diff, $list_files_to_upgrade);
-
-            $filesToMoveToTheBeginning = array(
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'ClassLoader.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_classmap.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_files.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_namespaces.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_psr4.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_real.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_static.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'include_paths.php',
-                DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer',
-                DIRECTORY_SEPARATOR.'vendor',
-            );
-
-            foreach ($filesToMoveToTheBeginning as $file) {
-                if ($key = array_search($file, $list_files_to_upgrade)) {
-                    unset($list_files_to_upgrade[$key]);
-                    $list_files_to_upgrade = array_merge(array($file), $list_files_to_upgrade);
-                }
-            }
-
-            // save in a serialized array in UpgradeFileNames::toUpgradeFileList
-            $this->upgradeClass->getFileConfigurationStorage()->save($list_files_to_upgrade, UpgradeFileNames::toUpgradeFileList);
-            $this->upgradeClass->nextParams['filesToUpgrade'] = UpgradeFileNames::toUpgradeFileList;
-            $total_files_to_upgrade = count($list_files_to_upgrade);
-
-            if ($total_files_to_upgrade == 0) {
-                $this->logger->error($this->upgradeClass->getTranslator()->trans('[ERROR] Unable to find files to upgrade.', array(), 'Modules.Autoupgrade.Admin'));
-                $this->upgradeClass->next = 'error';
-                return false;
-            }
-            $this->logger->info($this->upgradeClass->getTranslator()->trans('%s files will be upgraded.', array($total_files_to_upgrade), 'Modules.Autoupgrade.Admin'));
-            $this->upgradeClass->next = 'upgradeFiles';
-            $this->upgradeClass->stepDone = false;
-            return true;
+            return $this->warmUp();
         }
 
         // later we could choose between _PS_ROOT_DIR_ or _PS_TEST_DIR_
@@ -146,6 +80,120 @@ class UpgradeFiles extends AbstractTask
             @unlink(_PS_ROOT_DIR_.DIRECTORY_SEPARATOR. 'app'.DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR.'dev'.DIRECTORY_SEPARATOR.'class_index.php');
             @unlink(_PS_ROOT_DIR_.DIRECTORY_SEPARATOR. 'app'.DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR.'prod'.DIRECTORY_SEPARATOR.'class_index.php');
         }
+        return true;
+    }
+
+    /**
+     * list files to upgrade and return it as array
+     * TODO: This method needs probably to be moved in FilesystemAdapter
+     *
+     * @param string $dir
+     * @return number of files found
+     */
+    protected function listFilesToUpgrade($dir)
+    {
+        $list = array();
+        if (!is_dir($dir)) {
+            $this->logger->error($this->trans('[ERROR] %s does not exist or is not a directory.', array($dir), 'Modules.Autoupgrade.Admin'));
+            $this->logger->info($this->trans('Nothing has been extracted. It seems the unzipping step has been skipped.', array(), 'Modules.Autoupgrade.Admin'));
+            $this->upgradeClass->next = 'error';
+            return false;
+        }
+
+        $allFiles = scandir($dir);
+        foreach ($allFiles as $file) {
+            $fullPath = $dir.DIRECTORY_SEPARATOR.$file;
+
+            if ($this->upgradeClass->getFilesystemAdapter()->isFileSkipped($file, $fullPath, "upgrade")) {
+                if (!in_array($file, array('.', '..'))) {
+                    $this->logger->debug($this->trans('File %s is preserved', array($file), 'Modules.Autoupgrade.Admin'));
+                }
+                continue;
+            }
+            $list[] = str_replace($this->upgradeClass->latestRootDir, '', $fullPath);
+            if (is_dir($fullPath) && strpos($dir.DIRECTORY_SEPARATOR.$file, 'install') === false) {
+                $list = array_merge($list, $this->listFilesToUpgrade($fullPath));
+            }
+        }
+        return $list;
+    }
+
+    /**
+     * First call of this task needs a warmup, where we load the files list to be upgraded.
+     * 
+     * @return boolean
+     */
+    protected function warmUp()
+    {
+        $admin_dir = str_replace($this->upgradeClass->prodRootDir.DIRECTORY_SEPARATOR, '', $this->upgradeClass->adminDir);
+        if (file_exists($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'admin')) {
+            rename($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'admin', $this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.$admin_dir);
+        } elseif (file_exists($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'admin-dev')) {
+            rename($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'admin-dev', $this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.$admin_dir);
+        }
+        if (file_exists($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'install-dev')) {
+            rename($this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'install-dev', $this->upgradeClass->latestRootDir.DIRECTORY_SEPARATOR.'install');
+        }
+        
+        // list saved in UpgradeFileNames::toUpgradeFileList
+        // get files differences (previously generated)
+        $admin_dir = trim(str_replace($this->upgradeClass->prodRootDir, '', $this->upgradeClass->adminDir), DIRECTORY_SEPARATOR);
+        $filepath_list_diff = $this->upgradeClass->autoupgradePath.DIRECTORY_SEPARATOR.UpgradeFileNames::diffFileList;
+        $list_files_diff = array();
+        if (file_exists($filepath_list_diff)) {
+            $list_files_diff = $this->upgradeClass->getFileConfigurationStorage()->load(UpgradeFileNames::diffFileList);
+            // only keep list of files to delete. The modified files will be listed with _listFilesToUpgrade
+            $list_files_diff = $list_files_diff['deleted'];
+            foreach ($list_files_diff as $k => $path) {
+                if (preg_match("#autoupgrade#", $path)) {
+                    unset($list_files_diff[$k]);
+                } else {
+                    $list_files_diff[$k] = str_replace('/'.'admin', '/'.$admin_dir, $path);
+                }
+            } // do not replace by DIRECTORY_SEPARATOR
+        }
+
+        if (!($list_files_to_upgrade = $this->listFilesToUpgrade($this->upgradeClass->latestRootDir))) {
+            return false;
+        }
+
+        // also add files to remove
+        $list_files_to_upgrade = array_merge($list_files_diff, $list_files_to_upgrade);
+
+        $filesToMoveToTheBeginning = array(
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'ClassLoader.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_classmap.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_files.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_namespaces.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_psr4.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_real.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'autoload_static.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer'.DIRECTORY_SEPARATOR.'include_paths.php',
+            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer',
+            DIRECTORY_SEPARATOR.'vendor',
+        );
+
+        foreach ($filesToMoveToTheBeginning as $file) {
+            if ($key = array_search($file, $list_files_to_upgrade)) {
+                unset($list_files_to_upgrade[$key]);
+                $list_files_to_upgrade = array_merge(array($file), $list_files_to_upgrade);
+            }
+        }
+
+        // save in a serialized array in UpgradeFileNames::toUpgradeFileList
+        $this->upgradeClass->getFileConfigurationStorage()->save($list_files_to_upgrade, UpgradeFileNames::toUpgradeFileList);
+        $this->upgradeClass->nextParams['filesToUpgrade'] = UpgradeFileNames::toUpgradeFileList;
+        $total_files_to_upgrade = count($list_files_to_upgrade);
+
+        if ($total_files_to_upgrade == 0) {
+            $this->logger->error($this->upgradeClass->getTranslator()->trans('[ERROR] Unable to find files to upgrade.', array(), 'Modules.Autoupgrade.Admin'));
+            $this->upgradeClass->next = 'error';
+            return false;
+        }
+        $this->logger->info($this->upgradeClass->getTranslator()->trans('%s files will be upgraded.', array($total_files_to_upgrade), 'Modules.Autoupgrade.Admin'));
+        $this->upgradeClass->next = 'upgradeFiles';
+        $this->upgradeClass->stepDone = false;
         return true;
     }
 }
