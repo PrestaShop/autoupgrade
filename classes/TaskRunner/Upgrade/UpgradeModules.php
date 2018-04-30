cb9bb9a72d98da79ef3b367b62658876e3657fd7
Thomas Nabord <thomas.nabord@prestashop.com>
2018-04-30 09:53:58 +0100

    Check version before symfony adapter

--- classes/TaskRunner/Upgrade/UpgradeModules.php
+++ classes/TaskRunner/Upgrade/UpgradeModules.php
@@ -1,9 +1,9 @@
 <?php
 /*
  * 2007-2018 PrestaShop
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
@@ -11,13 +11,13 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
- * 
+ *
  * DISCLAIMER
- * 
+ *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to http://www.prestashop.com for more information.
- * 
+ *
  *  @author PrestaShop SA <contact@prestashop.com>
  *  @copyright  2007-2018 PrestaShop SA
  *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
@@ -30,6 +30,7 @@ use PrestaShop\Module\AutoUpgrade\UpgradeException;
 use PrestaShop\Module\AutoUpgrade\Parameters\UpgradeFileNames;
 use PrestaShop\Module\AutoUpgrade\TaskRunner\AbstractTask;
 use PrestaShop\Module\AutoUpgrade\UpgradeTools\FilesystemAdapter;
+use PrestaShop\Module\AutoUpgrade\UpgradeTools\SymfonyAdapter;
 use PrestaShop\Module\AutoUpgrade\UpgradeContainer;
 
 /**
@@ -147,6 +148,11 @@ class UpgradeModules extends AbstractTask
         if ($total_modules_to_upgrade) {
             $this->logger->info($this->translator->trans('%s modules will be upgraded.', array($total_modules_to_upgrade), 'Modules.Autoupgrade.Admin'));
         }
+
+        if (version_compare($this->container->getState()->getInstallVersion(), '1.7.0.0', '>=')) {
+            (new SymfonyAdapter())->clearMigrationCache();
+        }
+
         $this->stepDone = false;
         $this->next = 'upgradeModules';
         return true;
