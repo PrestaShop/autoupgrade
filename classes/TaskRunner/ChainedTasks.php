13f5dd19a7653528e8b1a61ebea804bad4c6278e
Thomas Nabord <thomas.nabord@prestashop.com>
2018-08-07 12:13:55 +0200

    Implement new abstract class ChainedTasks

--- classes/TaskRunner/ChainedTasks.php
+++ classes/TaskRunner/ChainedTasks.php
@@ -28,16 +28,13 @@
 namespace PrestaShop\Module\AutoUpgrade\TaskRunner;
 
 use PrestaShop\Module\AutoUpgrade\AjaxResponse;
-use PrestaShop\Module\AutoUpgrade\UpgradeContainer;
 use PrestaShop\Module\AutoUpgrade\UpgradeTools\TaskRepository;
 
 /**
- * Execute the whole upgrade process in a single request.
+ * Execute the whole process in a single request, useful in CLI.
  */
-class AllUpgradeTasks extends AbstractTask
+abstract class ChainedTasks extends AbstractTask
 {
-    const initialTask = 'upgradeNow';
-
     private $step = self::initialTask;
 
     public function run()
@@ -61,29 +58,11 @@ class AllUpgradeTasks extends AbstractTask
 
     /**
      * Customize the execution context with several options
-     * > action: Replace the initial step to run
-     * > channel: Makes a specific upgrade (minor, major etc.)
-     * > data: Loads an encoded array of data coming from another request.
      *
      * @param array $options
      */
-    public function setOptions($options)
+    public function setOptions(array $options)
     {
-        if (!empty($options['action'])) {
-            $this->step = $options['action'];
-        }
-
-        if (!empty($options['channel'])) {
-            $this->container->getUpgradeConfiguration()->merge(array(
-                'channel' => $options['channel'],
-            ));
-            $this->container->getUpgrader()->channel = $options['channel'];
-            $this->container->getUpgrader()->checkPSVersion(true);
-        }
-
-        if (!empty($options['data'])) {
-            $this->container->getState()->importFromEncodedData($options['data']);
-        }
     }
 
     /**
@@ -106,41 +85,10 @@ class AllUpgradeTasks extends AbstractTask
 
     /**
      * For some steps, we may require a new request to be made.
-     * For instance, in case of obsolete autoloader or loaded classes after a file copy.
+     * Return true for stopping the process
      */
     protected function checkIfRestartRequested(AjaxResponse $response)
     {
-        if (!$response->getStepDone()) {
-            return false;
-        }
-
-        if (!in_array($this->step, array('upgradeFiles'))) {
-            return false;
-        }
-
-        $this->logger->info('Restart requested. Please run the following command to continue your upgrade:');
-        $args = $_SERVER['argv'];
-        foreach ($args as $key => $arg) {
-            if (strpos($arg, '--data') === 0) {
-                unset($args[$key]);
-            }
-        }
-        $this->logger->info('$ '.implode(' ', $args).' --action='.$response->getNext().' --data='.$this->getEncodedResponse());
-
-        return true;
-    }
-
-    /**
-     * Set default config on first run.
-     */
-    public function init()
-    {
-        if ($this->step === self::initialTask) {
-            parent::init();
-            $this->container->getState()->initDefault(
-                $this->container->getUpgrader(),
-                $this->container->getProperty(UpgradeContainer::PS_ROOT_PATH),
-                $this->container->getProperty(UpgradeContainer::PS_VERSION));
-        }
+        return false;
     }
 }
