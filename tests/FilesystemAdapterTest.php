496b25cb13c8a3483675a790ff674080c1d007ee
MathiasReker <mathias@reker.dk>
2018-12-21 00:58:38 +0100

    One concat space

--- tests/FilesystemAdapterTest.php
+++ tests/FilesystemAdapterTest.php
@@ -65,7 +65,7 @@ class FilesystemAdapterTest extends TestCase
         $this->assertTrue(
             $this->filesystemAdapter->isFileSkipped(
                 $file,
-                $this->container->getProperty(UpgradeContainer::LATEST_PATH).$fullpath,
+                $this->container->getProperty(UpgradeContainer::LATEST_PATH) . $fullpath,
                 $process,
                 $this->container->getProperty(UpgradeContainer::LATEST_PATH)
             ));
