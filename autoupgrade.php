7d951c18b7becb99ab6b4777b3a25a6bc5d66b71
Thomas Nabord <thomas.nabord@prestashop.com>
2019-04-18 17:02:32 +0100

    Bump version to v4.7.0

--- autoupgrade.php
+++ autoupgrade.php
@@ -31,7 +31,7 @@ class Autoupgrade extends Module
         $this->name = 'autoupgrade';
         $this->tab = 'administration';
         $this->author = 'PrestaShop';
-        $this->version = '4.6.0';
+        $this->version = '4.7.0';
         $this->need_instance = 1;
 
         $this->bootstrap = true;
