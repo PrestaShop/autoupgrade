758a302840295097e41265756c1e0c251c035c10
Jean-François Viguier <16720275+jf-viguier@users.noreply.github.com>
2019-06-14 15:48:24 +0200

    Check file exists

--- classes/PrestashopConfiguration.php
+++ classes/PrestashopConfiguration.php
@@ -78,7 +78,10 @@ class PrestashopConfiguration
             $this->psRootDir . '/app/AppKernel.php',
         );
         foreach ($files as $file) {
-            $version = $this->findPrestaShopVersionInFile(@file_get_contents($file));
+            if (!file_exists($file)) {
+                continue;
+            }
+            $version = $this->findPrestaShopVersionInFile(file_get_contents($file));
             if ($version) {
                 return $version;
             }
