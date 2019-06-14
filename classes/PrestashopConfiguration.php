e82540b1f048c47630844d42886d69751adc5f1d
Jean-François Viguier <16720275+jf-viguier@users.noreply.github.com>
2019-06-14 14:42:07 +0200

    Fix warning in 1.7

--- classes/PrestashopConfiguration.php
+++ classes/PrestashopConfiguration.php
@@ -78,7 +78,7 @@ class PrestashopConfiguration
             $this->psRootDir . '/app/AppKernel.php',
         );
         foreach ($files as $file) {
-            $version = $this->findPrestaShopVersionInFile(file_get_contents($file));
+            $version = $this->findPrestaShopVersionInFile(@file_get_contents($file));
             if ($version) {
                 return $version;
             }
