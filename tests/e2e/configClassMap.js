a09635a1338dfaf7d65955c1bb3f98c7769b1c1a
Boubker Bribri <boubker.bribri@prestashop.com>
2021-03-08 13:10:26 +0100

    Add configClassMap.js

---
+++ tests/e2e/configClassMap.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const basePath = path.resolve(__dirname);
+console.log(basePath);
+
+module.exports = [
+  {
+    file: 'BO/modules/autoupgrade/index.js',
+    versions: {
+      '1.7.4': `${basePath}/pages/BO/modules/autoupgrade/index.js`,
+      '1.7.5': `${basePath}/pages/BO/modules/autoupgrade/index.js`,
+      '1.7.6': `${basePath}/pages/BO/modules/autoupgrade/index.js`,
+      '1.7.7': `${basePath}/pages/BO/modules/autoupgrade/index.js`,
+    },
+  },
+];
