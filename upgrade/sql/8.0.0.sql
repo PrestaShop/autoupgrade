8132cdf63804341b837315dce0508a347c2382d1
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2022-02-03 10:48:34 +0100

    Add displayCheckoutBeforeConfirmation hook

--- upgrade/sql/8.0.0.sql
+++ upgrade/sql/8.0.0.sql
@@ -24,6 +24,7 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
   (NULL, 'actionCustomerLogoutBefore', 'Before customer logout', 'This hook allows you to execute code before customer logout', '1'),
   (NULL, 'actionCustomerLogoutAfter', 'After customer logout', 'This hook allows you to execute code after customer logout', '1'),
   (NULL, 'displayCheckoutSummaryTop', 'Cart summary top', 'This hook allows you to display new elements in top of cart summary', '1')
+  (NULL, 'displayCheckoutBeforeConfirmation', 'Show custom content before checkout confirmation', 'This hook allows you to display custom content at the end of checkout process', '1')
 ;
 
 ALTER TABLE `PREFIX_employee_session` ADD `date_upd` DATETIME NOT NULL AFTER `token`;
