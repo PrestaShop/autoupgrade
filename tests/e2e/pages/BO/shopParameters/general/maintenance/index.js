ff946540ed1ebba41e4d9af97f617d9f47d61f56
boubkerbribri <boubker.bribri@prestashop.com>
2022-04-05 10:00:28 +0200

    Add steps to disable shop before upgrade

---
+++ tests/e2e/pages/BO/shopParameters/general/maintenance/index.js
@@ -0,0 +1,36 @@
+// Get resolver
+const VersionSelectResolver = require('prestashop_test_lib/kernel/resolvers/versionSelectResolver');
+
+const configClassMap = require('@root/configClassMap.js');
+
+const versionSelectResolver = new VersionSelectResolver(global.PS_RESOLVER_VERSION.FROM, configClassMap);
+
+// Import BOBasePage
+const BoBasePage = versionSelectResolver.require('BO/BObasePage.js');
+
+class Maintenance extends BoBasePage {
+  constructor() {
+    super();
+
+    this.pageTitle = 'Maintenance';
+    this.storeStatusRadio = status => `#form_general_enable_shop_${status}, #form_enable_shop_${status}`;
+    this.saveButton = '.card-footer button';
+  }
+
+  // Methods
+
+  /**
+   * Set shop status
+   * @param page {Page} Browser tab
+   * @param status {boolean} Status for the shop
+   * @returns {Promise<void>}
+   */
+  async setShopStatus(page, status = true) {
+    // eslint-disable-next-line no-return-assign,no-param-reassign
+    await page.$eval(this.storeStatusRadio(status ? '1' : '0'), el => el.checked = true);
+    await this.clickAndWaitForNavigation(page, this.saveButton);
+    await page.screenshot({path: 'page.png', fullPage: true});
+  }
+}
+
+module.exports = new Maintenance();
