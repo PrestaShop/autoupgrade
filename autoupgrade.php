ad84c78293e5cd29f69a2b5d0c85edd79db707b0
matks <mathieu.ferment@prestashop.com>
2022-04-22 16:30:17 +0200

    Bump to 4.15.0

--- autoupgrade.php
+++ autoupgrade.php
@@ -36,7 +36,7 @@ class Autoupgrade extends Module
         $this->name = 'autoupgrade';
         $this->tab = 'administration';
         $this->author = 'PrestaShop';
-        $this->version = '4.14.0';
+        $this->version = '4.15.0';
         $this->need_instance = 1;
 
         $this->bootstrap = true;
