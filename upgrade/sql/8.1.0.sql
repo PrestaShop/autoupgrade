9aca99a8464c8b257b28bfce679c224b8338c7a9
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-11-03 13:33:27 +0100

    Update 8.1.0.sql for actionProductPriceCalculation hook

--- upgrade/sql/8.1.0.sql
+++ upgrade/sql/8.1.0.sql
@@ -3,3 +3,8 @@ SET NAMES 'utf8mb4';
 
 UPDATE `PREFIX_configuration` SET `value` = 'US/Pacific' WHERE `name` = 'PS_TIMEZONE' AND `value` = 'US/Pacific-New' LIMIT 1;
 DELETE FROM `PREFIX_timezone` WHERE `name` = 'US/Pacific-New';
+
+/* New hooks added for version 8.1.0 */
+INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
+  (NULL, 'actionProductPriceCalculation', 'Product price calculation', 'This hook allows you to override or change the result of low-level price calculation', '1'),
+;
