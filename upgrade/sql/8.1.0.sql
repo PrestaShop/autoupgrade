22b8fffd7cb62dcd19749c07e3908fad1c3f5585
AMIT KUMAR TIWARI <amitkr.tiwari139@webkul.in>
2022-12-24 16:54:02 +0530

    Add a display hook in the address step

--- upgrade/sql/8.1.0.sql
+++ upgrade/sql/8.1.0.sql
@@ -7,6 +7,9 @@ DELETE FROM `PREFIX_timezone` WHERE `name` = 'US/Pacific-New';
 INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
   (NULL, 'actionModifyFrontendSitemap', 'Add or remove links on sitemap page', 'This hook allows to modify links on sitemap page of your shop. Useful to improve indexation of your modules.', '1');
 
+INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
+  (NULL, 'displayAddressSelectorBottom', 'After address selection on checkout page', 'This hook is displayed after the address selection in checkout step.', '1');
+
 /* Default configuration for backorder, in order to keep behavior */
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES
   ('PS_ENABLE_BACKORDER_STATUS', '1', NOW(), NOW());
