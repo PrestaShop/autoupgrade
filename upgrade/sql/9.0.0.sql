844d9ea4c6cfad88a4c60163e1172784420e9a01
Hlavtox <daniel.hlavacek@hotmail.cz>
2023-07-30 13:51:13 +0200

    Move script to proper location

--- upgrade/sql/9.0.0.sql
+++ upgrade/sql/9.0.0.sql
@@ -6,3 +6,6 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
   (NULL, 'actionCartGetPackageShippingCost', 'After getting package shipping cost value', 'This hook is called in order to allow to modify package shipping cost', '1');
 
 ALTER TABLE `PREFIX_feature_flag` ADD `type` VARCHAR(64) DEFAULT 'env,dotenv,db' NOT NULL AFTER `name`;
+
+/* Increase size of customized data - https://github.com/PrestaShop/PrestaShop/pull/31109 */
+ALTER TABLE `PREFIX_customized_data` MODIFY `value` varchar(1024) NOT NULL;
