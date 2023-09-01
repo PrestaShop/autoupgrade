162f09a4b63c72778894f02c4b520dd07c000816
Hlavtox <daniel.hlavacek@hotmail.cz>
2023-09-01 17:52:15 +0200

    Use correct prefix

--- upgrade/php/ps_1770_update_charset.php
+++ upgrade/php/ps_1770_update_charset.php
@@ -65,19 +65,19 @@ function ps_1770_update_charset()
             );
         }
         $result &= Db::getInstance()->execute(
-            'ALTER TABLE `PREFIX_admin_filter` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci'
+            'ALTER TABLE ' . _DB_PREFIX_ . '`admin_filter` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci'
         );
     }
 
     if ($moduleHistoryTableExists) {
         $result &= Db::getInstance()->execute(
-            'ALTER TABLE `PREFIX_module_history` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci'
+            'ALTER TABLE ' . _DB_PREFIX_ . '`module_history` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci'
         );
     }
 
     if ($translationTableExists) {
         $result &= Db::getInstance()->execute(
-            'ALTER TABLE `PREFIX_translation` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci'
+            'ALTER TABLE ' . _DB_PREFIX_ . '`translation` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci'
         );
     }
 
