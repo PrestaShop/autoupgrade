de9f6ef3f54aa6593e19941fee932bae7a7d0cd8
Dominik Ulrich <czshaimagal@gmail.com>
2023-11-12 12:25:04 +0100

    Update 8.1.3.sql

--- upgrade/sql/8.1.3.sql
+++ upgrade/sql/8.1.3.sql
@@ -18,3 +18,9 @@ ALTER TABLE `PREFIX_product_group_reduction_cache` CHANGE `reduction` `reduction
 ALTER TABLE `PREFIX_stock_mvt` CHANGE `physical_quantity` `physical_quantity` INT(10) UNSIGNED NOT NULL;
 ALTER TABLE `PREFIX_group_reduction` CHANGE `reduction` `reduction` DECIMAL(5, 4) NOT NULL;
 ALTER TABLE `PREFIX_order_payment` CHANGE `amount` `amount` DECIMAL(20, 6) NOT NULL;
+
+/* Fixing duplicates for table "accessory" where can be duplicate records from older version of PrestaShop, because of missing PRIMARY index */
+CREATE TABLE `PREFIX_accessory_tmp` SELECT DISTINCT `id_product_1`, `id_product_2` FROM `PREFIX_accessory`;
+ALTER TABLE `PREFIX_accessory_tmp` ADD PRIMARY KEY (`id_product_1`, `id_product_2`);
+DROP TABLE `PREFIX_accessory`;
+RENAME TABLE `PREFIX_accessory_tmp` TO `PREFIX_accessory`;
