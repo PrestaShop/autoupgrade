4b075c2aa143f5d48f40ef13b52aec1f6557fcb4
Dominik Ulrich <czshaimagal@gmail.com>
2023-11-12 18:15:05 +0100

    Create ps_900_delete_obsolete_files_in_root_folder.php

---
+++ upgrade/php/ps_900_delete_obsolete_files_in_root_folder.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+function ps_900_delete_obsolete_files_in_root_folder()
+{
+    $obsolete_files = ['footer.php', 'header.php', 'images.inc.php'];
+    foreach ($obsolete_files as $file) {
+        $full_path_to_file = _PS_ROOT_DIR_ . '/' . $file;
+        if (file_exists($full_path_to_file) && preg_match("/displayAsDeprecated/", $full_path_to_file) && preg_match("/PrestaShop/", $full_path_to_file)) {
+            unlink($full_path_to_file);
+        }
+    }
+    return true;
+}
