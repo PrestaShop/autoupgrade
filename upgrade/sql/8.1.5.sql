e0f0b59bc288ac2d873ea75d563118996ea97c59
Hlavtox <daniel.hlavacek@hotmail.cz>
2024-02-20 15:20:47 +0100

    Update comment

--- upgrade/sql/8.1.5.sql
+++ upgrade/sql/8.1.5.sql
@@ -1,7 +1,7 @@
 SET SESSION sql_mode='';
 SET NAMES 'utf8mb4';
 
-/* Adds missing hook entries that have been added to 8.1.4 installer. */
+/* Adds missing hook entries that have been added to 8.1.5 installer. */
 INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
   (NULL, 'actionSubmitAccountBefore', 'Triggers before customer registers', 'Triggers after submitting registration form, before the registration process itself. Allows to modify result of this action.', '1'),
   (NULL, 'actionAuthenticationBefore', 'Triggers before customer logs in', 'Triggers after successful validation of login form, before the login process itself.', '1'),
