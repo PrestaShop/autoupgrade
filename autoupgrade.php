fecc648b3654b4cffb5c06362577d321443c9d29
morgan <pichat.morgan@gmail.com>
2024-05-22 10:03:10 +0200

    Bump version 5.0.2

--- autoupgrade.php
+++ autoupgrade.php
@@ -36,7 +36,7 @@ class Autoupgrade extends Module
         $this->name = 'autoupgrade';
         $this->tab = 'administration';
         $this->author = 'PrestaShop';
-        $this->version = '5.0.1';
+        $this->version = '5.0.2';
         $this->need_instance = 1;
 
         $this->bootstrap = true;
