fe11254ab2df612eb22c6853504eafe7a861a3ca
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-05-28 16:14:41 +0200

    Fix Orders sanity tests

--- tests/UI/campaigns/sanity/01_ordersBO/01_filterOrders.spec.ts
+++ tests/UI/campaigns/sanity/01_ordersBO/01_filterOrders.spec.ts
@@ -1,16 +1,18 @@
 import {
-    // Import utils
-    testContext,
-    // Import BO pages
-    boDashboardPage,
-    boLoginPage,
-    boOrdersPage,
-    // Import data
-    dataOrders,
-    dataOrderStatuses,
+  // Import utils
+  testContext,
+  // Import BO pages
+  boDashboardPage,
+  boLoginPage,
+  boOrdersPage,
+  // Import data
+  dataOrders,
+  dataOrderStatuses,
 } from '@prestashop-core/ui-testing';
 
-import {test, expect, Page, BrowserContext} from '@playwright/test';
+import {
+  test, expect, Page, BrowserContext,
+} from '@playwright/test';
 
 const baseContext: string = 'sanity_ordersBO_filterOrders';
 
@@ -20,86 +22,98 @@ const baseContext: string = 'sanity_ordersBO_filterOrders';
   Logout from the BO
  */
 test.describe('BO - Orders - Orders : Filter the Orders table by ID, REFERENCE, STATUS', () => {
-    let browserContext: BrowserContext;
-    let page: Page;
-    let numberOfOrders: number;
+  let browserContext: BrowserContext;
+  let page: Page;
+  let numberOfOrders: number;
 
-    test.beforeAll(async ({browser}) => {
-        browserContext = await browser.newContext();
-        page = await browserContext.newPage();
+  test.beforeAll(async ({browser}) => {
+    browserContext = await browser.newContext();
+    page = await browserContext.newPage();
+  });
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  // Steps
+  test('should login in BO', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'loginBO', baseContext);
+
+    await boLoginPage.goTo(page, global.BO.URL);
+    await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
+
+    const pageTitle = await boDashboardPage.getPageTitle(page);
+    expect(pageTitle).toContain(boDashboardPage.pageTitle);
+  });
+
+  test('should go to the \'Orders > Orders\' page', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'goToOrdersPage', baseContext);
+
+    await boDashboardPage.goToSubMenu(
+      page,
+      boDashboardPage.ordersParentLink,
+      boDashboardPage.ordersLink,
+    );
+    await boOrdersPage.closeSfToolBar(page);
+
+    const pageTitle = await boOrdersPage.getPageTitle(page);
+    await expect(pageTitle).toContain(boOrdersPage.pageTitle);
+  });
+
+  test('should reset all filters and get number of orders', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'resetFilters1', baseContext);
+
+    numberOfOrders = await boOrdersPage.resetAndGetNumberOfLines(page);
+    await expect(numberOfOrders).toBeGreaterThan(0);
+  });
+
+  const tests = [
+    {
+      args: {
+        identifier: 'filterId', filterType: 'input', filterBy: 'id_order', filterValue: dataOrders.order_4.id,
+      },
+    },
+    {
+      args: {
+        identifier: 'filterReference', filterType: 'input', filterBy: 'reference', filterValue: dataOrders.order_2.reference,
+      },
+    },
+    {
+      args: {
+        identifier: 'filterOsName', filterType: 'select', filterBy: 'osname', filterValue: dataOrderStatuses.paymentError.name,
+      },
+    },
+  ];
+
+  tests.forEach((tst) => {
+    test(`should filter the Orders table by '${tst.args.filterBy}' and check the result`, async () => {
+      await testContext.addContextItem(test.info(), 'testIdentifier', tst.args.identifier, baseContext);
+
+      await boOrdersPage.filterOrders(
+        page,
+        tst.args.filterType,
+        tst.args.filterBy,
+        tst.args.filterValue.toString(),
+      );
+
+      const textColumn = await boOrdersPage.getTextColumn(page, tst.args.filterBy, 1);
+      await expect(textColumn).toEqual(tst.args.filterValue.toString());
     });
-    test.afterAll(async () => {
-        await page.close();
+
+    test(`should reset filter by '${tst.args.filterBy}'`, async () => {
+      await testContext.addContextItem(test.info(), 'testIdentifier', `reset_${tst.args.identifier}`, baseContext);
+
+      const numberOfOrdersAfterReset = await boOrdersPage.resetAndGetNumberOfLines(page);
+      await expect(numberOfOrdersAfterReset).toEqual(numberOfOrders);
     });
+  });
 
-    // Steps
-    test('should login in BO', async () => {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'loginBO', baseContext);
+  // Logout from BO
+  test('should log out from BO', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'logoutBO', baseContext);
 
-        await boLoginPage.goTo(page, global.BO.URL);
-        await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
+    await boLoginPage.logoutBO(page);
 
-        const pageTitle = await boDashboardPage.getPageTitle(page);
-        expect(pageTitle).toContain(boDashboardPage.pageTitle);
-    });
-
-    test('should go to the \'Orders > Orders\' page', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'goToOrdersPage', baseContext);
-
-        await boDashboardPage.goToSubMenu(
-            page,
-            boDashboardPage.ordersParentLink,
-            boDashboardPage.ordersLink,
-        );
-        await boOrdersPage.closeSfToolBar(page);
-
-        const pageTitle = await boOrdersPage.getPageTitle(page);
-        await expect(pageTitle).toContain(boOrdersPage.pageTitle);
-    });
-
-    test('should reset all filters and get number of orders', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'resetFilters1', baseContext);
-
-        numberOfOrders = await boOrdersPage.resetAndGetNumberOfLines(page);
-        await expect(numberOfOrders).toBeGreaterThan(0);
-    });
-
-    const tests = [
-        {args: {identifier: 'filterId', filterType: 'input', filterBy: 'id_order', filterValue: dataOrders.order_4.id}},
-        {args: {identifier: 'filterReference', filterType: 'input', filterBy: 'reference', filterValue: dataOrders.order_2.reference}},
-        {args: {identifier: 'filterOsName', filterType: 'select', filterBy: 'osname', filterValue: dataOrderStatuses.paymentError.name}},
-    ];
-
-    tests.forEach((tst) => {
-        test(`should filter the Orders table by '${tst.args.filterBy}' and check the result`, async function () {
-            await testContext.addContextItem(test.info(), 'testIdentifier', tst.args.identifier, baseContext);
-
-            await boOrdersPage.filterOrders(
-                page,
-                tst.args.filterType,
-                tst.args.filterBy,
-                tst.args.filterValue.toString(),
-            );
-
-            const textColumn = await boOrdersPage.getTextColumn(page, tst.args.filterBy, 1);
-            await expect(textColumn).toEqual(tst.args.filterValue.toString());
-        });
-
-        test(`should reset filter by '${tst.args.filterBy}'`, async function () {
-            await testContext.addContextItem(test.info(), 'testIdentifier', `reset_${tst.args.identifier}`, baseContext);
-
-            const numberOfOrdersAfterReset = await boOrdersPage.resetAndGetNumberOfLines(page);
-            await expect(numberOfOrdersAfterReset).toEqual(numberOfOrders);
-        });
-    });
-
-    // Logout from BO
-    test('should log out from BO', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'logoutBO', baseContext);
-
-        await boLoginPage.logoutBO(page);
-
-        const pageTitle = await boLoginPage.getPageTitle(page);
-        expect(pageTitle).toContain(boLoginPage.pageTitle);
-    });
+    const pageTitle = await boLoginPage.getPageTitle(page);
+    expect(pageTitle).toContain(boLoginPage.pageTitle);
+  });
 });
--- tests/UI/campaigns/sanity/01_ordersBO/02_editOrder.spec.ts
+++ tests/UI/campaigns/sanity/01_ordersBO/02_editOrder.spec.ts
@@ -1,17 +1,18 @@
 import {
-    // Import utils
-    testContext,
-    // Import BO pages
-    boDashboardPage,
-    boLoginPage,
-    boOrdersPage,
-    boOrdersViewProductsBlockPage,
-    // Import data
-    dataOrders,
-    dataOrderStatuses,
+  // Import utils
+  testContext,
+  // Import BO pages
+  boDashboardPage,
+  boLoginPage,
+  boOrdersPage,
+  boOrdersViewProductsBlockPage,
+  // Import data
+  dataOrderStatuses,
 } from '@prestashop-core/ui-testing';
 
-import {test, expect, Page, BrowserContext} from '@playwright/test';
+import {
+  test, expect, Page, BrowserContext,
+} from '@playwright/test';
 
 const baseContext: string = 'sanity_ordersBO_editOrder';
 
@@ -21,73 +22,73 @@ const baseContext: string = 'sanity_ordersBO_editOrder';
   Logout from the BO
  */
 test.describe('BO - Orders - Orders : Edit Order BO', async () => {
-    let browserContext: BrowserContext;
-    let page: Page;
+  let browserContext: BrowserContext;
+  let page: Page;
 
-    // before and after functions
-    test.beforeAll(async ({browser}) => {
-        browserContext = await browser.newContext();
-        page = await browserContext.newPage();
-    });
-    test.afterAll(async () => {
-        await page.close();
-    });
+  // before and after functions
+  test.beforeAll(async ({browser}) => {
+    browserContext = await browser.newContext();
+    page = await browserContext.newPage();
+  });
+  test.afterAll(async () => {
+    await page.close();
+  });
 
-    // Steps
-    test('should login in BO', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'loginBO', baseContext);
+  // Steps
+  test('should login in BO', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'loginBO', baseContext);
 
-        await boLoginPage.goTo(page, global.BO.URL);
-        await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
+    await boLoginPage.goTo(page, global.BO.URL);
+    await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
 
-        const pageTitle = await boDashboardPage.getPageTitle(page);
-        expect(pageTitle).toContain(boDashboardPage.pageTitle);
-    });
+    const pageTitle = await boDashboardPage.getPageTitle(page);
+    expect(pageTitle).toContain(boDashboardPage.pageTitle);
+  });
 
-    test('should go to the \'Orders > Orders\' page', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'goToOrdersPage', baseContext);
+  test('should go to the \'Orders > Orders\' page', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'goToOrdersPage', baseContext);
 
-        await boDashboardPage.goToSubMenu(
-            page,
-            boDashboardPage.ordersParentLink,
-            boDashboardPage.ordersLink,
-        );
-        await boOrdersPage.closeSfToolBar(page);
+    await boDashboardPage.goToSubMenu(
+      page,
+      boDashboardPage.ordersParentLink,
+      boDashboardPage.ordersLink,
+    );
+    await boOrdersPage.closeSfToolBar(page);
 
-        const pageTitle = await boOrdersPage.getPageTitle(page);
-        expect(pageTitle).toContain(boOrdersPage.pageTitle);
-    });
+    const pageTitle = await boOrdersPage.getPageTitle(page);
+    expect(pageTitle).toContain(boOrdersPage.pageTitle);
+  });
 
-    test('should go to the first order page', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'goToFirstOrder', baseContext);
+  test('should go to the first order page', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'goToFirstOrder', baseContext);
 
-        await boOrdersPage.goToOrder(page, 1);
+    await boOrdersPage.goToOrder(page, 1);
 
-        const pageTitle = await boOrdersViewProductsBlockPage.getPageTitle(page);
-        expect(pageTitle).toContain(boOrdersViewProductsBlockPage.pageTitle);
-    });
+    const pageTitle = await boOrdersViewProductsBlockPage.getPageTitle(page);
+    expect(pageTitle).toContain(boOrdersViewProductsBlockPage.pageTitle);
+  });
 
-    test('should modify the product quantity and check the validation', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'editProductQuantity', baseContext);
-        
-        const newQuantity = await boOrdersViewProductsBlockPage.modifyProductQuantity(page, 1, 5);
-        expect(newQuantity, 'Quantity was not updated').toEqual(5);
-    });
+  test('should modify the product quantity and check the validation', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'editProductQuantity', baseContext);
 
-    test.skip('should modify the order status and check the validation', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'editOrderStatus', baseContext);
-        
-        const orderStatus = await boOrdersViewProductsBlockPage.modifyOrderStatus(page, dataOrderStatuses.paymentAccepted.name);
-        expect(orderStatus).toEqual(dataOrderStatuses.paymentAccepted.name);
-    });
+    const newQuantity = await boOrdersViewProductsBlockPage.modifyProductQuantity(page, 1, 5);
+    expect(newQuantity, 'Quantity was not updated').toEqual(5);
+  });
 
-    // Logout from BO
-    test('should log out from BO', async function () {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'logoutBO', baseContext);
+  test.skip('should modify the order status and check the validation', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'editOrderStatus', baseContext);
 
-        await boLoginPage.logoutBO(page);
+    const orderStatus = await boOrdersViewProductsBlockPage.modifyOrderStatus(page, dataOrderStatuses.paymentAccepted.name);
+    expect(orderStatus).toEqual(dataOrderStatuses.paymentAccepted.name);
+  });
 
-        const pageTitle = await boLoginPage.getPageTitle(page);
-        expect(pageTitle).toContain(boLoginPage.pageTitle);
-    });
+  // Logout from BO
+  test('should log out from BO', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'logoutBO', baseContext);
+
+    await boLoginPage.logoutBO(page);
+
+    const pageTitle = await boLoginPage.getPageTitle(page);
+    expect(pageTitle).toContain(boLoginPage.pageTitle);
+  });
 });
