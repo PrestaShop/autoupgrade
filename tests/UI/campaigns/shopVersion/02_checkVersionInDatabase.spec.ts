e68992aafc8b15f52bc3712f66799fcf0c80ba6b
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-06-05 16:13:22 +0200

    Add new tests to check PSVERSION

---
+++ tests/UI/campaigns/shopVersion/02_checkVersionInDatabase.spec.ts
@@ -0,0 +1,49 @@
+import {
+    // Import utils
+    testContext,
+} from '@prestashop-core/ui-testing';
+
+import {createConnection} from 'mysql2/promise';
+import type {Connection} from 'mysql2/promise';
+
+let dbConnection: Connection;
+
+import {
+    test, expect, Page, BrowserContext,
+} from '@playwright/test';
+
+const baseContext: string = 'shopVersion_checkVersionIndatabase';
+const psVersion = testContext.getPSVersion();
+
+test.describe('Check new shop version', () => {
+    const dbPrefix: string = global.INSTALL.DB_PREFIX;
+
+    test.beforeAll(async () => {
+        if (!global.GENERATE_FAILED_STEPS) {
+            dbConnection = await createConnection({
+                user: global.INSTALL.DB_USER,
+                password: global.INSTALL.DB_PASSWD,
+                host: global.INSTALL.DB_SERVER,
+                port: 3306,
+                database: global.INSTALL.DB_NAME,
+                connectionLimit: 5,
+            });
+        }
+    });
+    test.afterAll(async () => {
+        if (dbConnection) {
+            await dbConnection.end();
+        }
+    });
+
+    test('should check psVersion from the database', async () => {
+        await testContext.addContextItem(test.info(), 'testIdentifier', 'checkPsVersion', baseContext);
+
+        await dbConnection.execute(`TRUNCATE TABLE ${dbPrefix}stock_mvt`);
+
+        await dbConnection.query(`SELECT PS_VERSION_DB FROM ${dbPrefix}configuration`, function (result:string) {
+            console.log(result);
+          //  expect(result).toEqual(psVersion);
+        });
+    });
+});
