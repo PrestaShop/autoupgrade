8be09f195d3bed78261dfbf263ab4821f7b9c6bc
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-06-07 11:04:59 +0200

    Unskip checkVersionInDatabase

--- tests/UI/campaigns/shopVersion/02_checkVersionInDatabase.spec.ts
+++ tests/UI/campaigns/shopVersion/02_checkVersionInDatabase.spec.ts
@@ -1,49 +1,48 @@
 import {
-    // Import utils
-    testContext,
+  // Import utils
+  testContext,
 } from '@prestashop-core/ui-testing';
 
 import {createConnection} from 'mysql2/promise';
 import type {Connection} from 'mysql2/promise';
 
-let dbConnection: Connection;
-
 import {
-    test, expect, Page, BrowserContext,
+  test, expect, Page, BrowserContext,
 } from '@playwright/test';
+
+let dbConnection: Connection;
 
 const baseContext: string = 'shopVersion_checkVersionIndatabase';
 const psVersion = testContext.getPSVersion();
 
-test.describe.skip('Check new shop version', () => {
-    const dbPrefix: string = global.INSTALL.DB_PREFIX;
+test.describe('Check new shop version', () => {
+  const dbPrefix: string = global.INSTALL.DB_PREFIX;
 
-    test.beforeAll(async () => {
-        if (!global.GENERATE_FAILED_STEPS) {
-            dbConnection = await createConnection({
-                user: global.INSTALL.DB_USER,
-                password: global.INSTALL.DB_PASSWD,
-                host: global.INSTALL.DB_SERVER,
-                port: 3306,
-                database: global.INSTALL.DB_NAME,
-                connectionLimit: 5,
-            });
-        }
+  test.beforeAll(async () => {
+    if (!global.GENERATE_FAILED_STEPS) {
+      dbConnection = await createConnection({
+        user: global.INSTALL.DB_USER,
+        password: global.INSTALL.DB_PASSWD,
+        host: 'localhost',
+        port: 3306,
+        database: global.INSTALL.DB_NAME,
+        connectionLimit: 5,
+      });
+    }
+  });
+  test.afterAll(async () => {
+    if (dbConnection) {
+      await dbConnection.end();
+    }
+  });
+
+  test('should check psVersion from the database', async () => {
+    await testContext.addContextItem(test.info(), 'testIdentifier', 'checkPsVersion', baseContext);
+
+    await dbConnection.execute(`TRUNCATE TABLE ${dbPrefix}stock_mvt`);
+
+    await dbConnection.query(`SELECT PS_VERSION_DB FROM ${dbPrefix}configuration`, (result:string) => {
+      expect(result).toEqual(psVersion);
     });
-    test.afterAll(async () => {
-        if (dbConnection) {
-            await dbConnection.end();
-        }
-    });
-
-    test('should check psVersion from the database', async () => {
-        await testContext.addContextItem(test.info(), 'testIdentifier', 'checkPsVersion', baseContext);
-
-        await dbConnection.execute(`TRUNCATE TABLE ${dbPrefix}stock_mvt`);
-
-        await dbConnection.query(`SELECT PS_VERSION_DB FROM ${dbPrefix}configuration`, function (result:string) {
-            console.log(result);
-          //  expect(result).toEqual(psVersion);
-        });
-    });
+  });
 });
