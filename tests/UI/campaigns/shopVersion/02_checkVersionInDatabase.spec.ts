7c7877630416ebed877ee876ec99cdfaaec9d594
Progi1984 <franck.lefevre@prestashop.com>
2024-06-19 15:39:55 +0200

    Check new shop version : Fixed connection

--- tests/UI/campaigns/shopVersion/02_checkVersionInDatabase.spec.ts
+++ tests/UI/campaigns/shopVersion/02_checkVersionInDatabase.spec.ts
@@ -4,7 +4,7 @@ import {
 } from '@prestashop-core/ui-testing';
 
 import {createConnection} from 'mysql2/promise';
-import type {Connection} from 'mysql2/promise';
+import type {Connection, FieldPacket, RowDataPacket} from 'mysql2/promise';
 
 import {
   test, expect,
@@ -15,7 +15,7 @@ let dbConnection: Connection;
 const baseContext: string = 'shopVersion_checkVersionIndatabase';
 const psVersion = testContext.getPSVersion();
 
-test.describe.skip('Check new shop version', () => {
+test.describe('Check new shop version', () => {
   const dbPrefix: string = global.INSTALL.DB_PREFIX;
 
   test.beforeAll(async () => {
@@ -23,7 +23,7 @@ test.describe.skip('Check new shop version', () => {
       dbConnection = await createConnection({
         user: global.INSTALL.DB_USER,
         password: global.INSTALL.DB_PASSWD,
-        host: 'localhost',
+        host: global.INSTALL.DB_SERVER,
         port: 3306,
         database: global.INSTALL.DB_NAME,
         connectionLimit: 5,
@@ -39,10 +39,9 @@ test.describe.skip('Check new shop version', () => {
   test('should check psVersion from the database', async () => {
     await testContext.addContextItem(test.info(), 'testIdentifier', 'checkPsVersion', baseContext);
 
-    await dbConnection.execute(`TRUNCATE TABLE ${dbPrefix}stock_mvt`);
-
-    await dbConnection.query(`SELECT PS_VERSION_DB FROM ${dbPrefix}configuration`, (result:string) => {
-      expect(result).toEqual(psVersion);
-    });
+    const [resultRows]: [RowDataPacket[], FieldPacket[]] = await dbConnection.query(
+      `SELECT value FROM ${dbPrefix}configuration WHERE name = 'PS_VERSION_DB'`,
+    );
+    expect(resultRows[0]['value']).toEqual(psVersion);
   });
 });
