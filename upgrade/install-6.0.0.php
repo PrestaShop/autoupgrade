179111cc44d871720736c28004b3b695db886a25
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-07-05 15:53:13 +0200

    feat: update table

--- upgrade/install-6.0.0.php
+++ upgrade/install-6.0.0.php
@@ -34,5 +34,40 @@ if (!defined('_PS_VERSION_')) {
  */
 function upgrade_module_6_0_0($module)
 {
-    return $module->unregisterHook('dashboardZoneOne');
+    if (!$module->unregisterHook('dashboardZoneOne')) {
+        return false;
+    }
+
+    // Update the 'AdminSelfUpgrade' tab configuration
+    $id_tab = \Tab::getIdFromClassName('AdminSelfUpgrade');
+    if ($id_tab) {
+        $tab = new \Tab($id_tab);
+        $tab->id_parent = (int) \Tab::getIdFromClassName('CONFIGURE');
+        $tab->icon = 'upgrade';
+
+        foreach (\Language::getLanguages(false) as $lang) {
+            $tab->name[(int) $lang['id_lang']] = '1-Click Upgrade';
+        }
+
+        if (!$tab->save()) {
+            return false;
+        }
+    } else {
+        // If the tab doesn't exist, create it
+        $tab = new \Tab();
+        $tab->class_name = 'AdminSelfUpgrade';
+        $tab->module = 'autoupgrade';
+        $tab->id_parent = (int) \Tab::getIdFromClassName('CONFIGURE');
+        $tab->icon = 'upgrade';
+
+        foreach (\Language::getLanguages(false) as $lang) {
+            $tab->name[(int) $lang['id_lang']] = '1-Click Upgrade';
+        }
+
+        if (!$tab->save()) {
+            return false;
+        }
+    }
+
+    return true;
 }
