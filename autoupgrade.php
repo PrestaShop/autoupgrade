09bfdc6f04c1b2b005d283f5884fcb3c030f9068
Morgan Pichat <morgan.pichat@prestashop.com>
2024-07-15 16:31:17 +0200

    Bump module version from 5.0.3 to 6.0.0

--- autoupgrade.php
+++ autoupgrade.php
@@ -36,7 +36,7 @@ class Autoupgrade extends Module
         $this->name = 'autoupgrade';
         $this->tab = 'administration';
         $this->author = 'PrestaShop';
-        $this->version = '5.0.3';
+        $this->version = '6.0.0';
         $this->need_instance = 1;
 
         $this->bootstrap = true;
