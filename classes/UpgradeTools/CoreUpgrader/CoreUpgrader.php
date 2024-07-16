b2a3a1aaee9ac87587896ca675ca9142d5b4c4a8
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-07-16 17:00:55 +0200

    fix: test update trans

--- classes/UpgradeTools/CoreUpgrader/CoreUpgrader.php
+++ classes/UpgradeTools/CoreUpgrader/CoreUpgrader.php
@@ -135,7 +135,7 @@ abstract class CoreUpgrader
         $this->logger->info($this->container->getTranslator()->trans('Upgrading languages'));
         $this->upgradeLanguages();
 
-        $this->logger->info($this->container->getTranslator()->trans('Regenerating htaccesssx'));
+        $this->logger->info($this->container->getTranslator()->trans('Regenerating htaccesssxspp'));
         $this->generateHtaccess();
 
         $this->logger->info($this->container->getTranslator()->trans('Cleaning XML filess'));
