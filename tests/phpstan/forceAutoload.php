d04c66baa8ee0975c5cb8a0b612b5ddbc7ee9585
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-09-19 11:26:40 +0200

    fix: cs fixer

--- tests/phpstan/forceAutoload.php
+++ tests/phpstan/forceAutoload.php
@@ -4,7 +4,7 @@
  * This module usually runs without relying of PrestaShop classes.
  * During the initialization of PHPStan in a module context, PrestaShop Autoloader is always called
  * to make sure module calls are known.
- * 
+ *
  * To avoid signature mismatch reported by PHPStan, we force the loading of some of our classes in memory.
  */
 
