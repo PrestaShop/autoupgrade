82b2a7df529bbc3014ba123afad48df7d76c2ce8
Thomas Nabord <thomas.nabord@prestashop.com>
2024-10-02 18:36:25 +0200

    Force loading of files to avoid conflicts from PrestaShop dependencies

--- tests/phpstan/forceAutoload.php
+++ tests/phpstan/forceAutoload.php
@@ -11,3 +11,19 @@
 use Symfony\Component\Console\Output\Output;
 
 class_exists(Output::class);
+
+// We load Twig classes to avoid conflicts with old versions of PrestaShop providing old versions of the library.
+// Avoid Reflection error: Circular reference to class "Twig_Environment"
+require_once __DIR__ . '/../../vendor/twig/twig/src/Environment.php';
+
+// Avoid Reflection error: Circular reference to class "Twig\Error\Error"
+require_once __DIR__ . '/../../vendor/twig/twig/src/Error/Error.php';
+
+// Avoid Reflection error: Circular reference to class "Twig_ExtensionInterface"
+require_once __DIR__ . '/../../vendor/twig/twig/src/Extension/ExtensionInterface.php';
+require_once __DIR__ . '/../../vendor/twig/twig/src/Extension/AbstractExtension.php';
+
+// Avoid Reflection error: Circular reference to class "Twig_LoaderInterface"
+require_once __DIR__ . '/../../vendor/twig/twig/src/Loader/LoaderInterface.php';
+require_once __DIR__ . '/../../vendor/twig/twig/src/Loader/ExistsLoaderInterface.php';
+require_once __DIR__ . '/../../vendor/twig/twig/src/Loader/SourceContextLoaderInterface.php';
