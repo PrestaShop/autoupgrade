fd83a8067a6ec6de98504b2e90266441e58f9551
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-10-11 17:08:06 +0200

    feat: add check requirements again button handle

--- _dev/src/ts/pages/UpdatePageVersionChoice.ts
+++ _dev/src/ts/pages/UpdatePageVersionChoice.ts
@@ -5,109 +5,105 @@ import Hydration from '../utils/Hydration';
 export default class UpdatePageVersionChoice extends UpdatePage {
   protected stepCode = 'version-choice';
   private radioLoadingClass = 'radio--show-requirements-loader';
-  form?: HTMLFormElement;
-  onlineCardParent?: HTMLDivElement;
-  localCardParent?: HTMLDivElement;
-  submitButton?: HTMLButtonElement;
 
   constructor() {
     super();
-    const form = document.forms.namedItem('version_choice');
-    if (form) {
-      this.form = form;
-
-      this.onlineCardParent = document.getElementById('radio_card_online') as
-        | HTMLDivElement
-        | undefined;
-
-      this.localCardParent = document.getElementById('radio_card_archive') as
-        | HTMLDivElement
-        | undefined;
-
-      this.submitButton = Array.from(this.form.elements).find(
-        (element) => element instanceof HTMLButtonElement && element.type === 'submit'
-      ) as HTMLButtonElement | undefined;
-    }
-
-    console.log(this);
   }
 
-  public mount() {
+  // TODO : add check requirements again handle on click
+
+  public mount = () => {
     this.initStepper();
     if (this.form) {
-      this.form.addEventListener('change', this.handleSave.bind(this));
+      this.form.addEventListener('change', this.saveForm.bind(this));
       this.form.addEventListener('submit', this.handleSubmit);
       if (this.onlineCardParent) {
-        this.onlineCardParent.addEventListener(Hydration.hydrationEventName, this.handleHydration);
+        this.onlineCardParent.addEventListener(Hydration.hydrationEventName, this.handleHydrate);
       }
       if (this.localCardParent) {
-        this.localCardParent.addEventListener(Hydration.hydrationEventName, this.handleHydration);
+        this.localCardParent.addEventListener(Hydration.hydrationEventName, this.handleHydrate);
       }
+      this.toggleNextButton();
+      this.addListenerToCheckRequirementsAgainButtons();
     }
-  }
+  };
 
   public beforeDestroy = () => {
     if (this.form) {
-      this.form.removeEventListener('change', this.handleSave);
+      this.form.removeEventListener('change', this.saveForm);
       this.form.removeEventListener('submit', this.handleSubmit);
       if (this.onlineCardParent) {
         this.onlineCardParent.removeEventListener(
           Hydration.hydrationEventName,
-          this.handleHydration
+          this.toggleNextButton
         );
       }
       if (this.localCardParent) {
         this.localCardParent.removeEventListener(
           Hydration.hydrationEventName,
-          this.handleHydration
+          this.toggleNextButton
         );
       }
+      this.checkRequirementsAgainButtons?.forEach((element) => {
+        element.removeEventListener('click', this.saveForm);
+      });
     }
   };
 
-  private sendForm(routeToSend: string) {
-    const formData = new FormData(this.form);
-    api.post(routeToSend, formData);
-  }
+  private sendForm = (routeToSend: string) => {
+    if (this.form) {
+      const formData = new FormData(this.form);
+      api.post(routeToSend, formData);
+    }
+  };
 
-  private handleHydration(event: Event) {
-    console.log('I M HYDRATED :', event);
-  }
+  private addListenerToCheckRequirementsAgainButtons = () => {
+    if (this.checkRequirementsAgainButtons?.length) {
+      this.checkRequirementsAgainButtons.forEach((element) => {
+        element.addEventListener('click', this.saveForm);
+      });
+    }
+  };
 
-  private handleSave() {
+  private handleHydrate = () => {
+    this.toggleNextButton();
+    this.addListenerToCheckRequirementsAgainButtons();
+  };
+
+  private toggleNextButton = () => {
+    if (this.currentChannelRequirementsAreOk) {
+      this.submitButton?.removeAttribute('disabled');
+    } else {
+      this.submitButton?.setAttribute('disabled', 'true');
+    }
+  };
+
+  private saveForm = () => {
     const routeToSave = this.form!.dataset.routeToSave;
 
     if (!routeToSave) {
       throw new Error('No route to save form provided. Impossible to save form.');
     }
 
-    const onlineInputElement = this.form!.elements.namedItem('online') as HTMLInputElement | null;
-    if (onlineInputElement && onlineInputElement.checked) {
-      onlineInputElement.classList.add(this.radioLoadingClass);
-      this.sendForm(routeToSave);
+    let currentInputCheck = null;
+
+    if (this.onlineInputIsChecked) {
+      currentInputCheck = this.onlineInputElement!;
     }
 
-    const localInputElement = this.form!.elements.namedItem('local') as HTMLInputElement | null;
-    const archiveZipSelectElement = this.form!.elements.namedItem(
-      'archive_zip'
-    ) as HTMLSelectElement | null;
-    const archiveXmlSelectElement = this.form!.elements.namedItem(
-      'archive_xml'
-    ) as HTMLSelectElement | null;
-    if (
-      localInputElement &&
-      archiveZipSelectElement &&
-      archiveXmlSelectElement &&
-      localInputElement.checked &&
-      archiveZipSelectElement.value &&
-      archiveXmlSelectElement.value
-    ) {
-      localInputElement.classList.add(this.radioLoadingClass);
+    if (this.localInputIsCheckAndFullFilled) {
+      currentInputCheck = this.localInputElement!;
+    }
+
+    if (currentInputCheck) {
+      currentInputCheck.removeAttribute('data-requirements-are-ok');
+      this.toggleNextButton();
+      currentInputCheck.classList.add(this.radioLoadingClass);
       this.sendForm(routeToSave);
     }
-  }
+  };
 
-  private handleSubmit(event: Event) {
+  private handleSubmit = (event: Event) => {
     event.preventDefault();
     const routeToSubmit = this.form!.dataset.routeToSubmit;
 
@@ -116,5 +112,92 @@ export default class UpdatePageVersionChoice extends UpdatePage {
     }
 
     this.sendForm(routeToSubmit);
+  };
+
+  // global form
+  private get form(): HTMLFormElement | null {
+    return document.forms.namedItem('version_choice');
+  }
+
+  private get submitButton(): HTMLButtonElement | undefined {
+    return this.form
+      ? (Array.from(this.form.elements).find(
+          (element) => element instanceof HTMLButtonElement && element.type === 'submit'
+        ) as HTMLButtonElement | undefined)
+      : undefined;
+  }
+
+  private get currentChannelRequirementsAreOk(): boolean {
+    if (this.onlineInputIsChecked) {
+      return this.onlineInputElement!.dataset.requirementsAreOk === '1';
+    }
+    if (this.localInputIsCheckAndFullFilled) {
+      return this.localInputElement!.dataset.requirementsAreOk === '1';
+    }
+    return false;
+  }
+
+  private get checkRequirementsAgainButtons(): HTMLButtonElement[] | undefined {
+    return this.form
+      ? (Array.from(this.form.elements).filter(
+          (element): element is HTMLButtonElement =>
+            element instanceof HTMLButtonElement &&
+            element.dataset.action === 'check-requirements-again'
+        ) as HTMLButtonElement[])
+      : undefined;
+  }
+
+  // online option
+  private get onlineCardParent(): HTMLDivElement | undefined {
+    return document.getElementById('radio_card_online') as HTMLDivElement | undefined;
+  }
+
+  private get onlineInputElement(): HTMLInputElement | undefined {
+    return this.form
+      ? (this.form!.elements.namedItem('online') as HTMLInputElement | undefined)
+      : undefined;
+  }
+
+  private get onlineInputIsChecked(): boolean {
+    return (this.onlineInputElement && this.onlineInputElement.checked) || false;
+  }
+
+  // local option
+  private get localCardParent(): HTMLDivElement | undefined {
+    return document.getElementById('radio_card_archive') as HTMLDivElement | undefined;
+  }
+
+  private get localInputElement(): HTMLInputElement | undefined {
+    return this.form
+      ? (this.form!.elements.namedItem('local') as HTMLInputElement | undefined)
+      : undefined;
+  }
+
+  private get localInputIsChecked(): boolean {
+    return this.localInputElement?.checked || false;
+  }
+
+  private get archiveZipSelectElement(): HTMLSelectElement | undefined {
+    return this.form
+      ? (this.form!.elements.namedItem('archive_zip') as HTMLSelectElement | undefined)
+      : undefined;
+  }
+
+  private get archiveZipIsFilled(): boolean {
+    return !!this.archiveZipSelectElement?.value;
+  }
+
+  private get archiveXmlSelectElement(): HTMLSelectElement | undefined {
+    return this.form
+      ? (this.form!.elements.namedItem('archive_xml') as HTMLSelectElement | undefined)
+      : undefined;
+  }
+
+  private get archiveXmlIsFilled(): boolean {
+    return (this.archiveXmlSelectElement && !!this.archiveXmlSelectElement.value) || false;
+  }
+
+  private get localInputIsCheckAndFullFilled(): boolean {
+    return this.localInputIsChecked && this.archiveZipIsFilled && this.archiveXmlIsFilled;
   }
 }
