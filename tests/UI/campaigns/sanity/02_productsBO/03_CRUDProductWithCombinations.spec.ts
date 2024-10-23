e6b4b86921a27ed9e83db213e94a2f58283d6260
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-10-23 17:56:18 +0200

    Rebase

--- tests/UI/campaigns/sanity/02_productsBO/03_CRUDProductWithCombinations.spec.ts
+++ tests/UI/campaigns/sanity/02_productsBO/03_CRUDProductWithCombinations.spec.ts
@@ -60,7 +60,7 @@ test.describe('BO - Catalog - Products : CRUD product with combinations', async 
     attributes: [
       {
         name: 'color',
-        values: ['Gray', 'Grey', 'Taupe', 'Red'],
+        values: ['Gray', 'Grey', 'Grey', 'Taupe', 'Red'],
       },
       {
         name: 'size',
