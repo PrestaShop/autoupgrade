c0f1ec9ebdacaa62e119681597ef4a115be290fb
Karlis Suvi <45097959+ks129@users.noreply.github.com>
2024-10-24 11:21:26 +0000

    Move feature value order configuration addition from PHP to SQL

--- upgrade/sql/9.0.0.sql
+++ upgrade/sql/9.0.0.sql
@@ -202,3 +202,4 @@ ALTER TABLE `PREFIX_order_payment` CHANGE `order_reference` `order_reference` VA
 /* Feature value position */
 /* https://github.com/PrestaShop/PrestaShop/pull/37042 */
 /* PHP:add_column('feature_value', 'position', 'int(10) unsigned NOT NULL DEFAULT \'0\''); */;
+INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_FEATURE_VALUES_ORDER', 'name', NOW(), NOW());
