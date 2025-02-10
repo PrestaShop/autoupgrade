6a8a06d2cb423282a0050378bd811d9c55091267
tblivet <thibault.blivet@prestashop.com>
2025-02-10 17:49:52 +0100

    feat: dialogs theme

--- _dev/src/ts/notifications.ts
+++ _dev/src/ts/notifications.ts
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const dialog = document.getElementById('dialog-update-notification');
 
   if (dialog instanceof HTMLDialogElement) {
-    dialog.showModal();
+    setTimeout(() => {
+      dialog.showModal();
+    }, 1500);
   }
   
   const closeButton = dialog?.querySelector('[data-dismiss="dialog"]');
