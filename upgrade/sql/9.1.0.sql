3eebb894084aa854584ccd5e10d1d141b062781c
morgan <pichat.morgan@gmail.com>
2025-04-02 11:44:11 +0200

    Add discount feature flag

--- upgrade/sql/9.1.0.sql
+++ upgrade/sql/9.1.0.sql
@@ -4,4 +4,5 @@ SET NAMES 'utf8mb4';
 /* Insert new feature flags introduced for the newly improved shipment system */
 /* https://github.com/PrestaShop/PrestaShop/pull/38040 */
 INSERT INTO `PREFIX_feature_flag` (`name`, `type`, `label_wording`, `label_domain`, `description_wording`, `description_domain`, `state`, `stability`) VALUES
-  ('improved_shipment', 'env,dotenv,db', 'Improved shipment', 'Admin.Advparameters.Feature', 'Enable / Disable the newly improved shipment system', 'Admin.Advparameters.Help', 0, 'beta');
+  ('improved_shipment', 'env,dotenv,db', 'Improved shipment', 'Admin.Advparameters.Feature', 'Enable / Disable the newly improved shipment system', 'Admin.Advparameters.Help', 0, 'beta'),
+  ('discount', 'env,dotenv,db', 'Discount', 'Admin.Advparameters.Feature', 'Enable / Disable the new discount system.', 'Admin.Advparameters.Help', 0, 'beta');
