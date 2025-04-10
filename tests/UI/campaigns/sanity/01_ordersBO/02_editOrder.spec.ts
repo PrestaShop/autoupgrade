d056db54e205ac4bf0fc273930dab26d59cd720a
dparis <david.paris@prestashop.com>
2025-04-10 15:49:26 +0200

    refactoring

--- tests/UI/campaigns/sanity/01_ordersBO/02_editOrder.spec.ts
+++ tests/UI/campaigns/sanity/01_ordersBO/02_editOrder.spec.ts
@@ -30,7 +30,7 @@ import {
 import setupSmtpConfigTest from 'commonTests/smtp';
 
 import {
-  test, expect, Page, BrowserContext,
+  test, expect,
 } from '@playwright/test';
 
 /*
@@ -38,32 +38,21 @@ Connect to the BO
 Edit the first order
 Logout from the BO
 */
-test.describe('BO - Orders - Orders : Edit Order BO', async () => {
-  let browserContext: BrowserContext;
-  let page: Page;
-
-  // before and after functions
-  test.beforeAll(async ({browser}) => {
-    browserContext = await browser.newContext();
-    page = await browserContext.newPage();
-  });
-  test.afterAll(async () => {
-    await page.close();
+test('BO - Orders - Orders : Edit Order BO', async ({page}) => {
+  await test.step('PRE-TEST: Setup SMTP config', async () => {
+    // Pre-Condition: Setup config SMTP
+    await setupSmtpConfigTest(page);
   });
 
-  // Pre-Condition: Setup config SMTP
-  setupSmtpConfigTest();
+  // await test.step('should login in BO', async () => {
+  //   await boLoginPage.goTo(page, global.BO.URL);
+  //   await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
+  //
+  //   const pageTitle = await boDashboardPage.getPageTitle(page);
+  //   expect(pageTitle).toContain(boDashboardPage.pageTitle);
+  // });
 
-  // Steps
-  test('should login in BO', async () => {
-    await boLoginPage.goTo(page, global.BO.URL);
-    await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
-
-    const pageTitle = await boDashboardPage.getPageTitle(page);
-    expect(pageTitle).toContain(boDashboardPage.pageTitle);
-  });
-
-  test('should go to the \'Orders > Orders\' page', async () => {
+  await test.step('should go to the \'Orders > Orders\' page', async () => {
     await boDashboardPage.goToSubMenu(
       page,
       boDashboardPage.ordersParentLink,
@@ -75,25 +64,25 @@ test.describe('BO - Orders - Orders : Edit Order BO', async () => {
     expect(pageTitle).toContain(boOrdersPage.pageTitle);
   });
 
-  test('should go to the first order page', async () => {
+  await test.step('should go to the first order page', async () => {
     await boOrdersPage.goToOrder(page, 1);
 
     const pageTitle = await boOrdersViewBlockProductsPage.getPageTitle(page);
     expect(pageTitle).toContain(boOrdersViewBlockProductsPage.pageTitle);
   });
 
-  test('should modify the product quantity and check the validation', async () => {
+  await test.step('should modify the product quantity and check the validation', async () => {
     const newQuantity = await boOrdersViewBlockProductsPage.modifyProductQuantity(page, 1, 5);
     expect(newQuantity, 'Quantity was not updated').toEqual(5);
   });
 
-  test('should modify the order status and check the validation', async () => {
+  await test.step('should modify the order status and check the validation', async () => {
     const orderStatus = await boOrdersViewBasePage.modifyOrderStatus(page, dataOrderStatuses.paymentAccepted.name);
     expect(orderStatus).toEqual(dataOrderStatuses.paymentAccepted.name);
   });
 
   // Logout from BO
-  test('should log out from BO', async () => {
+  await test.step('should log out from BO', async () => {
     await boLoginPage.logoutBO(page);
 
     const pageTitle = await boLoginPage.getPageTitle(page);
