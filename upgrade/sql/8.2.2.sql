b8022580a773290f44e67d54aef0d60ceda80dfc
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-05-12 21:46:03 +0100

    Add 'actionDuplicateCartData' hook

---
+++ upgrade/sql/8.2.2.sql
@@ -0,0 +1,6 @@
+SET SESSION sql_mode='';
+SET NAMES 'utf8mb4';
+
+INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
+  (NULL, 'actionDuplicateCartData','Cart Duplication','This hook is triggered after all the cart related data has been duplicated', '1')
+ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `description` = VALUES(`description`);
