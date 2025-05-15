b1288b7c949ac920f4266a5bfcd4d0e0fb34b579
Jean-François Viguier <16720275+jf-viguier@users.noreply.github.com>
2025-05-15 12:50:26 +0200

    Add hook displayCartExtraProductInfo

--- upgrade/sql/8.2.2.sql
+++ upgrade/sql/8.2.2.sql
@@ -4,3 +4,6 @@ SET NAMES 'utf8mb4';
 INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
   (NULL, 'actionDuplicateCartData','Cart Duplication','This hook is triggered after all the cart related data has been duplicated', '1')
 ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `description` = VALUES(`description`);
+INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
+  (NULL, 'displayCartExtraProductInfo','Extra information in shopping cart product line','This hook adds extra information to the product lines, in the shopping cart', '1')
+ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `description` = VALUES(`description`);
