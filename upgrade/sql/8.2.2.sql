197804d597dd36fb713a740c0917cb4399d9a576
Jean-François Viguier <16720275+jf-viguier@users.noreply.github.com>
2025-05-16 14:02:46 +0200

    merge requests
    Co-authored-by: Thomas N <Quetzacoalt91@users.noreply.github.com>

--- upgrade/sql/8.2.2.sql
+++ upgrade/sql/8.2.2.sql
@@ -2,8 +2,6 @@ SET SESSION sql_mode='';
 SET NAMES 'utf8mb4';
 
 INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
-  (NULL, 'actionDuplicateCartData','Cart Duplication','This hook is triggered after all the cart related data has been duplicated', '1')
-ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `description` = VALUES(`description`);
-INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
+  (NULL, 'actionDuplicateCartData','Cart Duplication','This hook is triggered after all the cart related data has been duplicated', '1'),
   (NULL, 'displayCartExtraProductInfo','Extra information in shopping cart product line','This hook adds extra information to the product lines, in the shopping cart', '1')
 ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `description` = VALUES(`description`);
