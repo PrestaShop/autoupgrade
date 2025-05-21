d9a3ae1469aa10c5bddcba210ecb63eaa579c477
Alexis Guyomar <alexis.guyomar@prestashop.com>
2025-05-21 17:39:40 +0200

    fix: add log with invalid iso code for 1.7

--- classes/UpgradeTools/CoreUpgrader/CoreUpgrader17.php
+++ classes/UpgradeTools/CoreUpgrader/CoreUpgrader17.php
@@ -46,6 +46,8 @@ class CoreUpgrader17 extends CoreUpgrader
         $isoCode = $lang['iso_code'];
 
         if (!\Validate::isLangIsoCode($isoCode) || !\Language::getLangDetails($isoCode)) {
+            $this->logger->debug($this->container->getTranslator()->trans('%lang% is not a valid iso code, skipping', ['%lang%' => $isoCode]));
+
             return;
         }
         $errorsLanguage = [];
