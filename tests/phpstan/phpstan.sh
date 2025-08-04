5ae3e0c216869559144b66dd7ca3d7318f9e076a
Thomas Nabord <thomas.nabord@prestashop.com>
2025-08-04 14:20:09 +0200

    Update to PHPStan v2

--- tests/phpstan/phpstan.sh
+++ tests/phpstan/phpstan.sh
@@ -35,7 +35,7 @@ docker run --rm --volumes-from temp-ps \
        -e _PS_ROOT_DIR_=/var/www/html \
        --workdir=/var/www/html/modules/autoupgrade \
        --entrypoint=/var/www/html/modules/autoupgrade/tests/vendor/bin/phpstan \
-       prestashop/base:7.4-apache \
+       prestashop/prestashop:$PS_VERSION \
        analyse \
        --configuration=/var/www/html/modules/autoupgrade/tests/phpstan/phpstan-$PS_VERSION.neon \
        "${@:2}"
