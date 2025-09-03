2160ff7f8a261131805227165ff468b743169bb5
mattgoud <matthias.goudjil@prestashop.com>
2025-09-03 11:50:28 +0200

    [ps-9.0.1][db-configuration-table] add new value PS_MIN_LOGGER_LEVEL_IN_DB (related issue PrestaShop/issues/38139, related PR PrestaShop/pull/39442)

--- upgrade/sql/9.0.1.sql
+++ upgrade/sql/9.0.1.sql
@@ -1,7 +1,14 @@
 SET SESSION sql_mode='';
 SET NAMES 'utf8mb4';
 
+-- https://github.com/PrestaShop/PrestaShop/pull/39277
+-- Add a new hook to allow modules to modify the HTMLPurifier configuration
 INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
-  -- https://github.com/PrestaShop/PrestaShop/pull/39277
   (NULL, 'actionModifyHtmlPurifierConfig', 'Called when configuring HTMLPurifier', 'Allows modules to modify the HTMLPurifier definition by adding custom allowed HTML elements or attributes during Tools::purifyHTML().', '1')
 ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `description` = VALUES(`description`);
+
+-- https://github.com/PrestaShop/PrestaShop/pull/39442
+-- Add the configuration value PS_MIN_LOGGER_LEVEL_IN_DB (default value = 1)
+INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES
+  ('PS_MIN_LOGGER_LEVEL_IN_DB', '1', NOW(), NOW())
+ON DUPLICATE KEY UPDATE `value` = VALUES(`value`);
