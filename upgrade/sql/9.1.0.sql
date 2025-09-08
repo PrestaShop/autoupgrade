0036519e6219d35a0400c39ecffd37ef7b6a5d0f
Progi1984 <franck@lefevre.dev>
2025-09-08 09:07:30 +0200

    Added the feature flag for Tag migration

--- upgrade/sql/9.1.0.sql
+++ upgrade/sql/9.1.0.sql
@@ -1,11 +1,16 @@
 SET SESSION sql_mode='';
 SET NAMES 'utf8mb4';
 
-/* Insert new feature flags introduced for the newly improved shipment system */
-/* https://github.com/PrestaShop/PrestaShop/pull/38040 */
+/* 
+  Insert new feature flags introduced for the newly improved shipment system
+  https://github.com/PrestaShop/PrestaShop/pull/38040
+  Insert new feature flags introduced for the migration of tag page
+  https://github.com/PrestaShop/PrestaShop/pull/39516
+*/
 INSERT INTO `PREFIX_feature_flag` (`name`, `type`, `label_wording`, `label_domain`, `description_wording`, `description_domain`, `state`, `stability`) VALUES
   ('improved_shipment', 'env,dotenv,db', 'Improved shipment', 'Admin.Advparameters.Feature', 'Enable / Disable the newly improved shipment system', 'Admin.Advparameters.Help', 0, 'beta'),
-  ('discount', 'env,dotenv,db', 'Discount', 'Admin.Advparameters.Feature', 'Enable / Disable the new discount system.', 'Admin.Advparameters.Help', 0, 'beta');
+  ('discount', 'env,dotenv,db', 'Discount', 'Admin.Advparameters.Feature', 'Enable / Disable the new discount system.', 'Admin.Advparameters.Help', 0, 'beta'),
+  ('tag', 'env,dotenv,db', 'Tag', 'Admin.Advparameters.Feature', 'Enable / Disable the tag page.', 'Admin.Advparameters.Help', 0, 'beta');
 
 /* Add a new field to cart_rule */
 /* https://github.com/PrestaShop/PrestaShop/pull/37911/ */
