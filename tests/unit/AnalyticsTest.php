317aec14697f3c04d00ad385554c2f0fd58827ef
Alexis Guyomar <alexis.guyomar@prestashop.com>
2025-09-22 11:18:15 +0200

    fix: php unit

--- tests/unit/AnalyticsTest.php
+++ tests/unit/AnalyticsTest.php
@@ -58,6 +58,7 @@ class AnalyticsTest extends TestCase
             UpgradeConfiguration::PS_AUTOUP_KEEP_IMAGES => false,
             UpgradeConfiguration::CHANNEL => UpgradeConfiguration::CHANNEL_LOCAL,
             UpgradeConfiguration::ARCHIVE_ZIP => 'zip.zip',
+            UpgradeConfiguration::UPDATE_TYPE => 'patch'
         ]);
         $configurationStorage->save($updateConfiguration);
 
@@ -109,6 +110,7 @@ class AnalyticsTest extends TestCase
                     'disable_non_native_modules' => false,
                     'regenerate_customized_email_templates' => true,
                     'regenerate_rtl_stylesheet' => false,
+                    'update_type' => 'patch'
                 ],
             ],
             $analytics->getProperties(Analytics::WITH_UPDATE_PROPERTIES)
