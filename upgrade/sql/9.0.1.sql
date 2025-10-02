6b3f371816a34cde4595ba7ceac27cce764b73f4
Thomas Nabord <thomas.nabord@prestashop.com>
2025-10-02 17:27:59 +0100

    Add missing changes for PS 9.0.1 and 9.0.0 (with catch-up script)

--- upgrade/sql/9.0.1.sql
+++ upgrade/sql/9.0.1.sql
@@ -5,7 +5,9 @@ INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`
   -- https://github.com/PrestaShop/PrestaShop/pull/39366
   (NULL, 'actionCheckoutStepRenderTemplate','Modify the parameters of the checkout step template rendering','This hook is called when rendering every checkout step template', '1'),
   -- https://github.com/PrestaShop/PrestaShop/pull/39277
-  (NULL, 'actionModifyHtmlPurifierConfig', 'Called when configuring HTMLPurifier', 'Allows modules to modify the HTMLPurifier definition by adding custom allowed HTML elements or attributes during Tools::purifyHTML().', '1')
+  (NULL, 'actionModifyHtmlPurifierConfig', 'Called when configuring HTMLPurifier', 'Allows modules to modify the HTMLPurifier definition by adding custom allowed HTML elements or attributes during Tools::purifyHTML().', '1'),
+  -- https://github.com/PrestaShop/PrestaShop/pull/38487
+  (NULL, 'actionGetPdfTemplateObject', 'Get PDF template object', 'This hook allows to recieve a PDF template object from modules', '1')
 ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `description` = VALUES(`description`);
 
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES
