703f079604630ab9ed069168e77e46019a349ec7
Thomas Nabord <thomas.nabord@prestashop.com>
2025-10-07 14:07:05 +0100

    Introduce a safe mode of PrestaShop to avoid loading modules

--- classes/UpgradeContainer.php
+++ classes/UpgradeContainer.php
@@ -58,6 +58,7 @@ use PrestaShop\Module\AutoUpgrade\UpgradeTools\CacheCleaner;
 use PrestaShop\Module\AutoUpgrade\UpgradeTools\FileFilter;
 use PrestaShop\Module\AutoUpgrade\UpgradeTools\FilesystemAdapter;
 use PrestaShop\Module\AutoUpgrade\UpgradeTools\Module\ModuleAdapter;
+use PrestaShop\Module\AutoUpgrade\UpgradeTools\Module\SafeMode;
 use PrestaShop\Module\AutoUpgrade\UpgradeTools\Module\Source\Provider\AbstractModuleSourceProvider;
 use PrestaShop\Module\AutoUpgrade\UpgradeTools\Module\Source\Provider\ComposerSourceProvider;
 use PrestaShop\Module\AutoUpgrade\UpgradeTools\Module\Source\Provider\LocalSourceProvider;
@@ -216,6 +217,9 @@ class UpgradeContainer
 
     /** @var PrestashopVersionService */
     private $prestashopVersionService;
+
+    /** @var SafeMode */
+    private $safeMode;
 
     /** @var UpgradeSelfCheck */
     private $upgradeSelfCheck;
@@ -637,6 +641,15 @@ class UpgradeContainer
         }
 
         return $this->restoreState;
+    }
+
+    public function getSafeMode(): SafeMode
+    {
+        if (null === $this->safeMode) {
+            $this->safeMode = new SafeMode($this->getDb());
+        }
+
+        return $this->safeMode;
     }
 
     public function getUpdateState(): UpdateState
