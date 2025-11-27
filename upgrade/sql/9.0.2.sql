0fbdb7c7a63d0a00b65683cd54a9edf53db9f55d
Thomas Nabord <thomas.nabord@prestashop.com>
2025-11-27 09:43:41 +0000

    Fix column wrongly declared as NOT NULL instead of DEFAULT NULL

--- upgrade/sql/9.0.2.sql
+++ upgrade/sql/9.0.2.sql
@@ -3,7 +3,7 @@ SET NAMES 'utf8mb4';
 
 -- https://github.com/PrestaShop/PrestaShop/pull/39606
 ALTER TABLE `PREFIX_customer_message`
-    MODIFY `user_agent` varchar(255) NOT NULL;
+    MODIFY `user_agent` varchar(255) DEFAULT NULL;
 
 -- https://github.com/PrestaShop/PrestaShop/pull/39914
 INSERT IGNORE INTO `PREFIX_authorization_role` (`slug`) VALUES
