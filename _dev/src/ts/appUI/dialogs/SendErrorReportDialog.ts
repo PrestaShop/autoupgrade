365a6008787c4e0783fe7a2f9548d81c06c36054
Thomas Nabord <thomas.nabord@prestashop.com>
2026-01-16 17:00:51 +0000

    Add stack trace when possible in error report modal

--- _dev/src/ts/appUI/dialogs/SendErrorReportDialog.ts
+++ _dev/src/ts/appUI/dialogs/SendErrorReportDialog.ts
@@ -30,7 +30,7 @@ export default class SendErrorReportDialog extends DialogAbstract {
     this.form.addEventListener('submit', this.onSubmit);
 
     const errorMessageArea: HTMLTextAreaElement = this.form.querySelector('#errorMessage')!;
-    errorMessageArea.value = this.#lastErrorMessage;
+    errorMessageArea.value = this.#errorMessagePanelContents;
   };
 
   get form(): HTMLFormElement {
@@ -40,6 +40,14 @@ export default class SendErrorReportDialog extends DialogAbstract {
     }
 
     return form;
+  }
+
+  get #errorMessagePanelContents(): string {
+    if (this.#responseContents) {
+      return `${this.#lastErrorMessage}\n\n${JSON.parse(this.#responseContents)?.nextQuickInfo?.join('\n\n')}`;
+    }
+
+    return this.#lastErrorMessage;
   }
 
   get #lastErrorMessage(): string {
@@ -52,6 +60,10 @@ export default class SendErrorReportDialog extends DialogAbstract {
     return latestError;
   }
 
+  get #responseContents(): string | undefined {
+    return document.getElementById(ErrorPageBuilder.externalAdditionalContentsPanelId)?.textContent;
+  }
+
   onSubmit = async (event: SubmitEvent) => {
     event.preventDefault();
 
@@ -59,9 +71,7 @@ export default class SendErrorReportDialog extends DialogAbstract {
       logs: this.#getLogs(),
       other: new Map<string, string>()
     };
-    const responseContents = document.getElementById(
-      ErrorPageBuilder.externalAdditionalContentsPanelId
-    )?.textContent;
+    const responseContents = this.#responseContents;
     if (responseContents) {
       attachments.other.set('response_raw.txt', responseContents);
     }
