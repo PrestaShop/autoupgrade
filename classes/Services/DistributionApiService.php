8800997d6f1244f3f0228db71ff7dd967524324a
Thomas Nabord <thomas.nabord@prestashop.com>
2026-01-29 16:34:45 +0000

    Module compat with PS versions are nullable

--- classes/Services/DistributionApiService.php
+++ classes/Services/DistributionApiService.php
@@ -58,7 +58,7 @@ class DistributionApiService
     /**
      * @param string $prestashopVersion
      *
-     * @return array<string, array{display_name: string, tab: string, description: string, author: string, version: string, prestashop_min_version: string, prestashop_max_version: string, download_url: string, icon: string}>
+     * @return array<string, array{display_name: string, tab: string, description: string, author: string, version: string, prestashop_min_version: string|null, prestashop_max_version: string|null, download_url: string, icon: string}>
      *
      * @throws DistributionApiException
      */
