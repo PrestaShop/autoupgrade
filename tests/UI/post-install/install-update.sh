eeab2cdd6f90fa800f1fb21b13dfdf04c6096804
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2026-02-12 10:11:29 +0100

    Fix install update script

--- tests/UI/post-install/install-update.sh
+++ tests/UI/post-install/install-update.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
-set +e
-
-for i in {1..7}; do
-    echo "⏳ Attempting $i to install the autoupgrade module"
-
-    result=$(php -r '
-        require_once "/var/www/html/config/config.inc.php";
-        require_once "/var/www/html/init.php";
-
-        if (!Module::isInstalled("autoupgrade")) {
-            $module = Module::getInstanceByName("autoupgrade");
-            if (!$module) {
-                echo "⚠️ Autoupgrade module not found\n";
-                exit(1);
-            }
-            if ($module->install()) {
-                echo "✅ Autoupgrade module installed and activated !\n";
-                exit(0);
-            } else {
-                var_dump($module->getError());
-                echo "❌ Module installation failed.";
-            }
-        } else {
-            echo "Module already installed..\n";
-            exit(0);
-        }
-    ')
-
-    echo "$result";
-    if [[ $result == *"installed"* ]]; then
-      break
-    fi
-
-    sleep 2
-done
-
 set -e
+
+php -r '
+require_once "/var/www/html/config/config.inc.php";
+require_once "/var/www/html/init.php";
+
+$module = Module::getInstanceByName("autoupgrade");
+$module->install();
+$module->enable();
+
+echo "Module installed\n";
