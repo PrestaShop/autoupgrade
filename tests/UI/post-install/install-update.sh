b3e94bce552a0cda220002eb21f8701b5c7b8dbb
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2026-02-12 14:22:10 +0100

    Fix install script

--- tests/UI/post-install/install-update.sh
+++ tests/UI/post-install/install-update.sh
@@ -6,7 +6,14 @@ require_once "/var/www/html/config/config.inc.php";
 require_once "/var/www/html/init.php";
 
 $module = Module::getInstanceByName("autoupgrade");
-$module->install();
-$module->enable();
 
-echo "Module installed\n";
+if ($module && !$module->installed) {
+    $module->install();
+}
+
+if ($module && !$module->active) {
+    $module->enable();
+}
+
+echo "Module installed and enabled\n";
+'
