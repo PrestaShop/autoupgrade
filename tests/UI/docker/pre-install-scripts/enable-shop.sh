39c036cb9f4aeb0966bcedf7db439fb7e988c128
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2026-02-16 16:26:48 +0100

    Test nightli classic

--- tests/UI/docker/pre-install-scripts/enable-shop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y --no-install-recommends xmlstarlet
-xmlstarlet ed --inplace -u '/entity_configuration/entities/configuration[@id="PS_SHOP_ENABLE"]/value' -v 1 /var/www/html/install/data/xml/configuration.xml
