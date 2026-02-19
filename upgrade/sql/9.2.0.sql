b31e0af24217278aad5d74058b06c89b83dc40cf
Carangeot <kevin.carangeot@prestashop.com>
2026-02-19 15:25:02 +0100

    feat: add feature flag for One-page checkout

---
+++ upgrade/sql/9.2.0.sql
@@ -0,0 +1,10 @@
+SET SESSION sql_mode='';
+SET NAMES 'utf8mb4';
+
+/*
+  Insert new feature flags introduced for the one-page checkout
+  https://github.com/PrestaShop/PrestaShop/pull/38040
+*/
+INSERT INTO `PREFIX_feature_flag` (`name`, `type`, `label_wording`, `label_domain`, `description_wording`, `description_domain`, `state`, `stability`) VALUES
+  ('one_page_checkout', 'env,dotenv,db', 'One-page checkout', 'Admin.Advparameters.Feature', 'Enable / Disable one-page checkout flow. All checkout steps (address, delivery, payment) are grouped on a single page to reduce friction and improve conversion rate.', 'Admin.Advparameters.Help', 0, 'beta');
+
