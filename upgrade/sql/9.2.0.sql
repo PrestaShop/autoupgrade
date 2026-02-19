61a42d85826731f2d08352fa96fd14d376cbf372
Kévin <109509097+kevin-carangeot@users.noreply.github.com>
2026-02-19 15:47:18 +0100

    Apply suggestion from @kevin-carangeot

--- upgrade/sql/9.2.0.sql
+++ upgrade/sql/9.2.0.sql
@@ -3,7 +3,7 @@ SET NAMES 'utf8mb4';
 
 /*
   Insert new feature flags introduced for the one-page checkout
-  https://github.com/PrestaShop/PrestaShop/pull/38040
+  https://github.com/PrestaShop/PrestaShop/pull/40796
 */
 INSERT INTO `PREFIX_feature_flag` (`name`, `type`, `label_wording`, `label_domain`, `description_wording`, `description_domain`, `state`, `stability`) VALUES
   ('one_page_checkout', 'env,dotenv,db', 'One-page checkout', 'Admin.Advparameters.Feature', 'Enable / Disable one-page checkout flow. All checkout steps (address, delivery, payment) are grouped on a single page to reduce friction and improve conversion rate.', 'Admin.Advparameters.Help', 0, 'beta');
