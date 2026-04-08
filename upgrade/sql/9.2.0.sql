cb6057b5d184baf7c26de30e2ac720fc44c0293e
Soledis - Clément Hervouet <clem.hervouet@live.fr>
2026-04-08 11:33:09 +0200

    remove enterprise_id column and its index from business_entity table in 9.2.0 upgrade script

--- upgrade/sql/9.2.0.sql
+++ upgrade/sql/9.2.0.sql
@@ -7,7 +7,6 @@ INSERT INTO `PREFIX_feature_flag` (`name`, `type`, `label_wording`, `label_domai
 CREATE TABLE `PREFIX_business_entity`
 (
     `id_business_entity`       INT UNSIGNED AUTO_INCREMENT                     NOT NULL,
-    `enterprise_id`            VARCHAR(255) NOT NULL,
     `external_ref`             VARCHAR(255) DEFAULT NULL,
     `name`                     VARCHAR(255) NOT NULL,
     `legal_name`               VARCHAR(255) DEFAULT NULL,
@@ -15,7 +14,6 @@ CREATE TABLE `PREFIX_business_entity`
     `status`                   ENUM ('pending','active','inactive','rejected') NOT NULL DEFAULT 'pending',
     `created_at`               DATETIME     NOT NULL,
     `updated_at`               DATETIME     NOT NULL,
-    INDEX                      `business_entity_enterprise_id_idx` (`enterprise_id`),
     INDEX                      `business_entity_external_ref_idx` (`external_ref`),
     PRIMARY KEY (`id_business_entity`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
